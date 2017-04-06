41d97c349a122b9e87635debaca9c320e847876b
Travis <tlgreg86@gmail.com>
2017-04-06 11:39:10 -0600

    Add everything

--- scripts/CompleteMe.js
+++ scripts/CompleteMe.js
@@ -1,38 +1,62 @@
 import Node from './Node';
+require ('locus');
 
 export default class CompleteMe {
   constructor () {
     this.root =  new Node(null)
-    this.dictionary = []
-    this.countWord;
+    this.count = 0;
   }
 
+
   insert (word) {
-    let currentNode = this.root;
     let wordArray = word.split('')
+    let currentNode = this.root;
 
-
-    wordArray.forEach((letter) => {
+    wordArray.forEach( letter => {
       let newNode = new Node(letter)
 
       if (!currentNode.children[letter]) {
         currentNode.children[letter] = newNode
       }
+
       currentNode = currentNode.children[letter]
     })
     currentNode.isWord = true;
-    this.count(word)
+    this.count++
   }
 
-  count (word) {
-    this.dictionary.push(word)
-    this.countWords = this.dictionary.length
+
+  count () {
+    return this.count;
   }
+
+
+  suggest (input) {
+    let suggestions = [];
+
+    return this.getWord(this.findNode(input), input, suggestions)
+  }
+
+
+  getWord (currentNode, input, suggestions) {
+    if (currentNode.isWord) {
+      suggestions.push(input);
+    }
+    let nodeKeys = Object.keys(currentNode.children)
+
+    nodeKeys.forEach( key => {
+      let nextNode = currentNode.children[key];
+
+      this.getWord(nextNode, input + key, suggestions)
+    })
+    return suggestions
+  }
+
 
   findNode (word) {
+    let currentNode = this.root;
     let charArray = word.split('')
     let currentChar = charArray.shift();
-    let currentNode = this.root;
 
     while (currentNode.children[currentChar]) {
 
@@ -40,19 +64,26 @@ export default class CompleteMe {
       currentChar = charArray.shift()
     }
     return currentNode;
+  }
+
+
+  populate (array) {
+    array.forEach((word) => {
+      this.insert(word);
+    });
 
   }
 
-  suggest () {
 
-  }
-
-  populate () {
-
-  }
-
-  select () {
-
-  }
+  // select (prefix, word) {
+  //   // need to get suggestions => []
+  //   let suggestions = this.suggest(prefix)
+  //   // find word in suggestions array => 'string'
+  //   suggestions.find( word => {
+  //     currentNode.children ===
+  //   })
+  //   // findNode('string') => object
+  //   // object.counter ++
+  // }
 
 }
--- tests/CompleteMe-test.js
+++ tests/CompleteMe-test.js
@@ -1,47 +1,88 @@
 import {assert} from 'chai';
-// import Node from '../scripts/Node';
 import CompleteMe from '../scripts/CompleteMe';
-// import locus from 'locus';
+const fs = require('fs');
 
-describe('Complete Me test', () => {
+require('locus');
+
+describe('CompleteMe test', () => {
   let completeMe = new CompleteMe()
 
-  it('Should have a class of CompleteMe', () => {
+  it('should have a class of CompleteMe', () => {
 
     assert.isFunction(CompleteMe)
   })
 
-  it('Should istantiate a new Node that is equal to null', () => {
+  it('should istantiate a new Node that is equal to null', () => {
 
     assert.equal(completeMe.root.letter, null)
   })
 
-  it('Should have a dictionary that is assigned an empty array', () => {
+  it('should allow me to insert nodes and return true at end of word', () => {
+    completeMe.insert('pass')
 
-    assert.deepEqual(completeMe.dictionary, [])
-  })
-
-  it('Should return true when word is complete', () => {
-    completeMe.insert('hat')
     assert.equal(
       completeMe.root.children
-      .h.children
+      .p.children
       .a.children
-      .t.isWord, true
+      .s.children
+      .s.isWord, true
     )
   })
 
-  it('Should count the number of words in the dictionary', () => {
-    completeMe.insert('bat')
-    completeMe.insert('cat')
-    assert.deepEqual(completeMe.countWords, completeMe.dictionary.length)
+  it('should count the number nodes', () => {
+    completeMe.insert('past')
+    completeMe.insert('pastor')
+    assert.equal(completeMe.count, 3)
   })
 
-  it('Should find a node', () => {
-    completeMe.insert('pig')
-    let foundNode = completeMe.findNode('pig')
-    
-    assert.equal(foundNode.letter, 'g')
+  it('should return children nodes that are words', () => {
+    completeMe.insert('dog')
+    completeMe.insert('dogged')
+
+    let suggestion1 = completeMe.suggest('do');
+    let suggestion2 = completeMe.suggest('pas');
+
+    assert.deepEqual(suggestion1, ['dog', 'dogged'])
+    assert.deepEqual(suggestion2, ['pass', 'past', 'pastor'])
   })
+
+  it('should find a node', () => {
+    completeMe.insert('pig')
+    completeMe.insert('tomato')
+
+    let foundNode1 = completeMe.findNode('pig')
+    let foundNode2 = completeMe.findNode('tom')
+
+    assert.equal(foundNode1.letter, 'g')
+    assert.equal(foundNode2.letter, 'm')
+  })
+
+  // it('should select a word from recommendations array', () => {
+  //   completeMe.select('do', 'dogged')
+  //
+  //   assert.deepEqual()
+  // })
+
+})
+
+describe('Dictionary test', () => {
+  let completeMe = new CompleteMe()
+  const text = '/usr/share/dict/words';
+  let array = fs.readFileSync(text).toString().trim().split('\n');
+
+  it('should populate a dictionary', () => {
+    completeMe.populate(array);
+    assert.deepEqual(completeMe.count, 235886);
+  });
+
+  it('should suggest words from dictionary', () => {
+
+    completeMe.populate(array);
+
+    let suggestion = completeMe.suggest("piz")
+
+    assert.deepEqual(suggestion, ["pize", "pizza", "pizzeria", "pizzicato", "pizzle"])
+  })
+
 
 })
